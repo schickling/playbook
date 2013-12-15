8cf6302fb3abfe2664f61bdd5398f38d4fee8462
Johannes Schickling <schickling.j@gmail.com>
2013-12-15 19:41:56 +0100

    Rough install workflow

--- lib/playbook/install.rb
+++ lib/playbook/install.rb
@@ -1,8 +1,25 @@
+require 'yaml'
+require 'git'
+
 class Install
     def self.run
+        
         unless File.exist?("playbooks.yml")
             puts "No \"playbooks.yml\" file found."
             exit 1
         end
+
+
+        playbooks = YAML.load_file("playbooks.yml")
+
+        FileUtils.mkdir_p "playbooks"
+        Dir.chdir "playbooks"
+
+        playbooks.each do |playbook|
+            FileUtils.rm_rf playbook.split('/').first
+            git_url = "https://github.com/#{playbook}.git"
+            repo = Git.clone(git_url, playbook)
+        end
+
     end
 end
